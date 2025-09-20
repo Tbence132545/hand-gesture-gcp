1fd90166278e68e8d91e1a963dbb88f1487dec93
Tbence132545 <71981467+Tbence132545@users.noreply.github.com>
2025-09-20 23:30:48 +0200

    Clarify TensorFlow version downgrade in comments
    Updated comment for clarity on TensorFlow version compatibility.

--- training/convert_model.py
+++ training/convert_model.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
-#I was using tensorflow 2.20 to train the model, but Vertex AI only accepts 2.15 and below. So with venv we can downgrade the version with this script
+#I was using tensorflow 2.20 to train the model, but Vertex AI only accepts 2.15 and below. So with a venv we can downgrade the version with this script
 #to be used with vertex ai
-# Load your existing H5 model
 model = tf.keras.models.load_model("C:/Users/User/Desktop/hand-gesture-gcp/models/hand_gesture_model_final.h5")
 
 # Save as TF SavedModel (compatible with Vertex AI TF2.15)
